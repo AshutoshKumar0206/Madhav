fa00cfbad30e9038aa5576eee1c68d2debff231f
aryayanroy <aryayanroy0@gmail.com>
2024-03-02 18:40:53 +0530

    python connect

--- main.py
+++ main.py
@@ -1,27 +1,13 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/create_file', methods=['POST'])
-def create_file():
-    if request.method == 'POST':
-        data = request.json  # Get JSON data from the request
-        name = data.get('name', 'default_name')  # Extract 'name' from JSON data, or use a default name if not provided
-
-        # Write JSON data to a text file
-        with open(f"{name}.txt", "w") as f:
-            f.write(json.dumps(data, indent=4))  # Convert JSON data to a string and write to file
-
-        # Return a response to acknowledge the successful request
-        return jsonify(data), 200
+@app.route('/process_transcript', methods=['POST'])
+def process_transcript():
+    transcript = "Your request is"+request.form['transcript']
+    return transcript
 
 if __name__ == '__main__':
-    app.run(
-        host='127.0.0.1',
-        port=5001,
-        debug=True
-    )
+    app.run(debug=True)
